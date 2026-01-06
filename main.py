8147364f7cff2ec38a98640723660ac5840b8074
neg9414 <neg9414@edu.neg.dk>
2026-01-06 22:28:31 +0100

    omskrivet

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
+from artworks import Artwork
 from database import Database
 
 db = Database()
 
-# Søgning på en bestemt person
-term = "marie van"
-print(db.search(term))
+# Gemmer en ny række
+a1 = Artwork(title="Stjerne Nat", artist="Van Gogh", year=1889)
+a1.id = db.save(a1)
+print("Saved:", a1)
 
-# Opret/create nyt maleri til min tabel
-db.insert(
-    id=78,
-    artist="marie van",
-    painting="bad",
-    year=2023,
-    genre="Impressionism",
-    image="noimage.jpg",
-    beskrivelse="Et af maries mest berømte værker."
-)
+# Henter en række til at aflæse
+fetched = db.load(a1.id)
+print("Loaded:", fetched)
 
-# Henter et bestemt maleri
-print(db.load())
-
-# Vis alle malerier
-print(db.load_all()) 
+# Hent alt
+all_artworks = db.load_all()
+for art in all_artworks:
+    print("Alle:", art)
